ffba572d1977de69d1fc49c9d267cf619ca77fa1
psilva1982 <severos1982@gmail.com>
2021-10-17 21:04:01 -0300

    feat: :sparkles: add ordering fields

--- backend/currency_converter/admin.py
+++
@@ -1,9 +0,0 @@
-from currency_converter.models.quote import Quote
-from django.contrib import admin
-
-
-class QuoteAdmin(admin.ModelAdmin):
-    list_display = ["id", "date", "symbol", "value"]
-
-
-admin.site.register(Quote, QuoteAdmin)
